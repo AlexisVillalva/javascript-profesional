68fbc390a5e671a20ade877ffc9117a917f37f3b
Alexis Villalva <v8.alexis.v8@gmail.com>
2021-01-07 23:31:49 -0700

    Agregado ejercicio practica con event loop

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,45 +1,45 @@
 function MediaPlayer(config) {
-  this.media = config.el;
-  // adding plugins
-  this.plugins = config.plugins || [];
+    this.media = config.el;
+    // adding plugins
+    this.plugins = config.plugins || [];
 
-  this._initPlugins();
+    this._initPlugins();
 }
 
 MediaPlayer.prototype._initPlugins = function () {
-  this.plugins.forEach((element) => {
-    element.run(this);
-  });
+    this.plugins.forEach((element) => {
+        element.run(this);
+    });
 };
 
 MediaPlayer.prototype.play = function () {
-  this.media.play();
+    this.media.play();
 };
 
 MediaPlayer.prototype.pause = function () {
-  this.media.pause();
+    this.media.pause();
 };
 
 MediaPlayer.prototype.togglePlay = function () {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
+    if (this.media.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
 };
 
 MediaPlayer.prototype.mutee = function () {
-  console.log("muted");
-  this.media.muted = true;
+    console.log("muted");
+    this.media.muted = true;
 };
 
 MediaPlayer.prototype.toggleMute = function () {
-  console.log("toggle mute");
-  if (this.media.muted) {
-    this.media.muted = false;
-  } else {
-    this.media.muted = true;
-  }
+    console.log("toggle mute");
+    if (this.media.muted) {
+        this.media.muted = false;
+    } else {
+        this.media.muted = true;
+    }
 };
 
 export default MediaPlayer;
