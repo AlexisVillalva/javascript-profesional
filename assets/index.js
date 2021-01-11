6f868fdb5bfe530148fd466fc97148c829dc65c5
Alexisv8 <v8.alexis.v8@gmail.com>
2021-01-10 20:59:07 -0700

    Added data type exercise in typescript

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,21 @@
 import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
     el: video,
-    plugins: [new AutoPlay()],
+    plugins: [new AutoPlay(), new AutoPause()],
 });
 
 const playButton = document.querySelector("#toggle-play-button");
 const muteButton = document.querySelector("#toggle-mute-button");
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+// trabajando con service workers para guardar la aplicacion en cache
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.js")
+        .catch((error) => console.log(error.message));
+}
